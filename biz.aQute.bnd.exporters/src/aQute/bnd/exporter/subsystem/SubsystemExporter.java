550608c0b403c46ffdf50c948751a30d5df37320
stbischof <stbischof@bipolis.org>
2018-05-22 20:01:56 +0200

    cleanup

--- biz.aQute.bnd.exporters/src/aQute/bnd/exporter/subsystem/SubsystemExporter.java
+++ biz.aQute.bnd.exporters/src/aQute/bnd/exporter/subsystem/SubsystemExporter.java
@@ -6,10 +6,12 @@ import java.io.File;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.jar.Attributes;
+import java.util.jar.JarInputStream;
 import java.util.jar.Manifest;
 
 import org.osgi.service.subsystem.SubsystemConstants;
@@ -33,11 +35,22 @@ import aQute.bnd.service.Strategy;
 import aQute.bnd.service.export.Exporter;
 import aQute.lib.collections.MultiMap;
 import aQute.lib.io.ByteBufferOutputStream;
+import aQute.lib.io.IO;
 
 @BndPlugin(name = "subsystem")
 public class SubsystemExporter implements Exporter {
 
-	private static final String	OSGI_INF_SUBSYSTEM_MF		= "OSGI-INF/SUBSYSTEM.MF";
+	String CONSTANT_MANIFEST_VERSION = "Manifest-Version";
+
+	Map<String, String> getDefaultsMap() {
+
+		Map<String, String> defaultsMap = new HashMap<>();
+		defaultsMap.put(SubsystemConstants.SUBSYSTEM_TYPE, SubsystemConstants.SUBSYSTEM_TYPE_FEATURE);
+		defaultsMap.put(SubsystemConstants.SUBSYSTEM_VERSION, "0.0.0");
+		defaultsMap.put(SubsystemConstants.SUBSYSTEM_MANIFESTVERSION, "1");
+		defaultsMap.put(CONSTANT_MANIFEST_VERSION, "1.0");
+		return defaultsMap;
+	}
 
 	@Override
 	public String[] getTypes() {
@@ -50,31 +63,80 @@ public class SubsystemExporter implements Exporter {
 	@Override
 	public Map.Entry<String, Resource> export(String type, final Project project, Map<String, String> options)
 		throws Exception {
+
 		Jar jar = new Jar(".");
 
 		project.addClose(jar);
 
-		Manifest manifest = new Manifest();
-		manifest.getMainAttributes()
-			.putValue("Manifest-Version", "1.0");
-		manifest.getMainAttributes()
-			.putValue("Subsystem-ManifestVersion", "1");
+		EasArchivType esaArchivType = null;
+		Boolean storeBundles = null;
+		String archiveContent = project.get("-archiveContentType");
 
-		List<Container> distro = project.getBundles(Strategy.LOWEST, project.getProperty(Constants.DISTRO),
-			Constants.DISTRO);
-		List<File> distroFiles = getBundles(distro, project);
-		List<File> files = getBundles(project.getRunbundles(), project);
+		esaArchivType = EasArchivType.byParameter(archiveContent);
+		if (esaArchivType == null) {
+			esaArchivType = EasArchivType.CONTENT;
+		}
+
+		Collection<Container> requireBundles = getRequiredBundles(project);
+		Collection<Container> runBundles = project.getRunbundles();
+
+		Collection<Container> bundles = null;
+		switch (esaArchivType) {
+			case NONE :
+				bundles = requireBundles;
+				storeBundles = false;
+				break;
+
+			case CONTENT :
+
+				bundles = requireBundles;
+				storeBundles = true;
+				break;
+
+			case ALL :
+
+				bundles = runBundles;
+				storeBundles = true;
+				break;
+
+			default :
+
+				throw new IllegalArgumentException("Unknown EsaArchivType: " + esaArchivType);
+
+		}
+
+		List<File> files = getBundles(bundles, project);
 
 		MultiMap<String, Attrs> imports = new MultiMap<>();
 		MultiMap<String, Attrs> exports = new MultiMap<>();
-		Parameters requirements = new Parameters();
-		Parameters capabilities = new Parameters();
+		MultiMap<String, Attrs> requireCapability = new MultiMap<>();
+		MultiMap<String, Attrs> provideCapability = new MultiMap<>();
 
 		for (File file : files) {
-			Domain domain = Domain.domain(file);
-			String bsn = domain.getBundleSymbolicName()
-				.getKey();
-			String version = domain.getBundleVersion();
+			ContainerType containerType = ContainerType.byFile(file);
+
+			Domain domain;
+			// default & last. Assume JAR
+			try (JarInputStream jin = new JarInputStream(IO.stream(file))) {
+				Manifest m = jin.getManifest();
+				if (m != null) {
+					domain = Domain.domain(m);
+
+				} else {
+					throw new Exception("Container could not be read: " + file.getName());
+				}
+			}
+
+			String csn = null;
+			Parameters p = domain.getParameters(containerType.getSymbolicNameKey());
+			if (!p.isEmpty()) {
+				csn = p.entrySet()
+					.iterator()
+					.next()
+					.getKey();
+
+			}
+			String version = domain.get(containerType.getVersionKey());
 
 			for (Entry<String, Attrs> e : domain.getImportPackage()
 				.entrySet()) {
@@ -86,39 +148,114 @@ public class SubsystemExporter implements Exporter {
 				exports.add(e.getKey(), e.getValue());
 			}
 
-			String path = bsn + "-" + version + ".jar";
-			jar.putResource(path, new FileResource(file));
+			for (Entry<String, Attrs> e : domain.getRequireCapability()
+				.entrySet()) {
+				requireCapability.add(e.getKey(), e.getValue());
+			}
+
+			for (Entry<String, Attrs> e : domain.getProvideCapability()
+				.entrySet()) {
+				provideCapability.add(e.getKey(), e.getValue());
+			}
+
+			if (storeBundles) {
+
+				String path = csn + "-" + version + "." + containerType.getExtension();
+				jar.putResource(path, new FileResource(file));
+			}
 		}
 
-		headers(project, manifest.getMainAttributes());
-
-		set(manifest.getMainAttributes(), SubsystemConstants.SUBSYSTEM_TYPE, type);
-
-		String ssn = project.getName();
-
-		Collection<String> bsns = project.getBsns();
-		if (bsns.size() > 0) {
-			ssn = bsns.iterator()
-				.next();
+		Manifest manifest = new Manifest();
+		// as overriding parameter from bndrun or bnd.bnd
+		// set imports exports requireCapability and provideCapability from
+		// embedded bundles
+		Parameters requirementParameter = project.getRequireCapability();
+		if (requirementParameter != null && !requirementParameter.isEmpty()) {
+			requireCapability.clear();
+			for (Entry<String, Attrs> e : requirementParameter.entrySet()) {
+				requireCapability.add(e.getKey(), e.getValue());
+			}
 		}
-		set(manifest.getMainAttributes(), SubsystemConstants.SUBSYSTEM_SYMBOLICNAME, ssn);
+		set(manifest.getMainAttributes(), org.osgi.framework.Constants.REQUIRE_CAPABILITY, requireCapability);
+
+		Parameters capabilityParameter = project.getProvideCapability();
+		if (capabilityParameter != null && !capabilityParameter.isEmpty()) {
+			provideCapability.clear();
+			for (Entry<String, Attrs> e : capabilityParameter.entrySet()) {
+				provideCapability.add(e.getKey(), e.getValue());
+			}
+		}
+		set(manifest.getMainAttributes(), org.osgi.framework.Constants.PROVIDE_CAPABILITY, provideCapability);
+
+		Parameters importParameter = project.getImportPackage();
+		if (importParameter != null && !importParameter.isEmpty()) {
+			if (type.equals(SubsystemConstants.SUBSYSTEM_TYPE_FEATURE)) {
+
+				throw new Exception("Modifying imports by using headers is not allowed for features");
+			}
+		} else {
+			set(manifest.getMainAttributes(), org.osgi.framework.Constants.IMPORT_PACKAGE, imports);
+		}
+
+		Parameters exportParameter = project.getExportPackage();
+		if (exportParameter != null && !exportParameter.isEmpty()) {} else {
+
+			set(manifest.getMainAttributes(), org.osgi.framework.Constants.EXPORT_PACKAGE, exports);
+		}
+
+		// set headers from bndrun if not set
+		headers(manifest.getMainAttributes(), project);
+
+		// set default headers if not set
+		for (Entry<String, String> entry : getDefaultsMap().entrySet()) {
+			set(manifest.getMainAttributes(), entry.getKey(), entry.getValue());
+		}
+
+		set(manifest.getMainAttributes(), SubsystemConstants.SUBSYSTEM_SYMBOLICNAME, project.getName()
+			.replace(".bndrun", ""));
 
 		ByteBufferOutputStream bout = new ByteBufferOutputStream();
 		manifest.write(bout);
-
-		jar.putResource(OSGI_INF_SUBSYSTEM_MF, new EmbeddedResource(bout.toByteBuffer(), 0L));
+		jar.putResource(ContainerType.SUBSYSTEM.getManifestUri(), new EmbeddedResource(bout.toByteBuffer(), 0));
 
 		final JarResource jarResource = new JarResource(jar, true);
-		final String name = ssn + ".esa";
+		final String name = project.getName() + ".esa";
 
 		return new SimpleEntry<>(name, jarResource);
 	}
 
-	private List<File> getBundles(Collection<Container> bundles, Processor reporter) throws Exception {
+	private void set(Attributes mainAttributes, String key, MultiMap<String, Attrs> multiMap) {
+
+		Parameters parameters = new Parameters();
+		List<Attrs> allattrs = new ArrayList<>();
+		for (Entry<String, List<Attrs>> entry : multiMap.entrySet()) {
+
+			for (Attrs attrs : entry.getValue()) {
+
+				parameters.add(entry.getKey(), attrs);
+			}
+		}
+		mainAttributes.putValue(key, parameters.toString());
+		// (key, allattrs);
+
+	}
+
+	private Collection<Container> getRequiredBundles(final Project project) throws Exception {
+		Collection<Attrs> attrs = project.getParameters(Constants.RUNREQUIRES, "bnd.identity")
+			.values();
+		String value = "";
+		for (Attrs attrs2 : attrs) {
+			value += attrs2.get("id");
+		}
+		List<Container> containers = project.getBundles(Strategy.HIGHEST, value, Constants.RUNREQUIRES);
+		return containers;
+	}
+
+	private List<File> getBundles(Collection<Container> containers, Processor reporter) throws Exception {
 		List<File> files = new ArrayList<>();
 
-		for (Container c : bundles) {
-			switch (c.getType()) {
+		for (Container container : containers) {
+			switch (container.getType()) {
 				case ERROR :
 					// skip, already reported
 					break;
@@ -126,23 +263,25 @@ public class SubsystemExporter implements Exporter {
 				case PROJECT :
 				case EXTERNAL :
 				case REPO :
-					files.add(c.getFile());
+					files.add(container.getFile());
 					break;
 				case LIBRARY :
-					c.contributeFiles(files, reporter);
+					container.contributeFiles(files, reporter);
 					break;
 			}
 		}
 		return files;
 	}
 
-	private void headers(final Project project, Attributes application) {
+	// puts all properties with a HEADER_PATTERN from the bndrun to the
+	// Attributes, if it is neccesary
+	private void headers(Attributes mainAttributes, final Project project) {
 		for (String key : project.getPropertyKeys(true)) {
 			if (!Verifier.HEADER_PATTERN.matcher(key)
 				.matches())
 				continue;
 
-			if (application.getValue(key) != null)
+			if (mainAttributes.getValue(key) != null)
 				continue;
 
 			String value = project.getProperty(key);
@@ -153,27 +292,26 @@ public class SubsystemExporter implements Exporter {
 			if (value.isEmpty() || Constants.EMPTY_HEADER.equals(value))
 				continue;
 
-			char c = value.charAt(0);
+			char c = key.charAt(0);
 			if (Character.isUpperCase(c))
-				application.putValue(key, value);
+				mainAttributes.putValue(key, value);
 		}
 		Instructions instructions = new Instructions(project.mergeProperties(REMOVEHEADERS));
-		Collection<Object> result = instructions.select(application.keySet(), false);
-		application.keySet()
+		Collection<Object> result = instructions.select(mainAttributes.keySet(), false);
+		mainAttributes.keySet()
 			.removeAll(result);
 	}
 
-	private void set(Attributes application, String key, String... values) {
-		if (application.getValue(key) != null)
+	// puts the values to the attributes if no other value is set by this key
+	private void set(Attributes mainAttributes, String key, String... values) {
+		if (mainAttributes.getValue(key) != null)
 			return;
 
 		for (String value : values) {
 			if (value != null) {
-				application.putValue(key, value);
+				mainAttributes.putValue(key, value);
 				return;
 			}
 		}
-
 	}
-
 }
